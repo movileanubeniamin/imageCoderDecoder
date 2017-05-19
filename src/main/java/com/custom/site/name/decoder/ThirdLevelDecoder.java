9a1386d2444d8ce93721072a795c6aaec7b5f404
bmovileanu <movileanubeniamin@gmail.com>
2017-05-19 12:26:01 +0200

    Fixed bug where vector.size is smaller than 7

--- src/main/java/com/custom/site/name/decoder/ThirdLevelDecoder.java
+++ src/main/java/com/custom/site/name/decoder/ThirdLevelDecoder.java
@@ -15,7 +15,7 @@ public class ThirdLevelDecoder {
       long[][] block = new long[8][8];
       List<Integer> posx = new ArrayList<Integer>();
       List<Integer> posy = new ArrayList<Integer>();
-
+      
       // build of posx
       for (int i = 0; i < block.length; i += 2) {
          int j;
@@ -51,7 +51,7 @@ public class ThirdLevelDecoder {
          for (j = i + 1; j < block.length; j++)
             posy.add(j);
       }
-
+      
       for (int i = 0; i < block.length; i++)
          for (int j = 0; j < block.length; j++)
             block[i][j] = 0;
@@ -59,10 +59,12 @@ public class ThirdLevelDecoder {
       int temp = 1;
       block[0][0] = vector.get(1);
       for (int i = 1;; i++) {
+         if (vector.size() <= 6)
+            break;
          // int tempI = i;
          // if ((tempI * 3 + 1) > (vector.size() - 1))
          // break;
-
+         
          i += vector.get(3 * temp - 1);
          // while (vector.get(tempI * 3 - 1) > 0 && (i * 3 + 1) < posx.size()) {
          // block[posx.get(i)][posy.get(i)] = 0;
@@ -73,21 +75,24 @@ public class ThirdLevelDecoder {
             System.out.println((i * 3 + 1) + "*" + vector.size());
          if (i == 23)
             System.out.println((i * 3 + 1) + "?" + vector.size());
-         block[posx.get(i)][posy.get(i)] = vector.get(3 * temp + 1);
+         try {
+            block[posx.get(i)][posy.get(i)] = vector.get(3 * temp + 1);
+         } catch (IndexOutOfBoundsException exception) {
+            System.out.println("asd");
+         }
          temp += 1;
          if ((temp * 3 + 1) > vector.size() - 1)
             break;
       }
       return block;
    }
-
-
-   public static List<long[][]> zigZagToList(List<List<Long>> zigZagList){
+   
+   public static List<long[][]> zigZagToList(List<List<Long>> zigZagList) {
       List<long[][]> unzigZagList = new ArrayList<long[][]>();
-      for (int i = 0; i < zigZagList.size(); i++){
+      for (int i = 0; i < zigZagList.size(); i++) {
          unzigZagList.add(decodeVector(zigZagList.get(i)));
       }
       return unzigZagList;
    }
-
+   
 }
