1a26b99b479ecca0097dc2b258b5261ccf266f14
bmovileanu <movileanubeniamin@gmail.com>
2017-05-19 12:09:46 +0200

    Fixed decoder issue

--- src/main/java/com/custom/site/name/decoder/ThirdLevelDecoder.java
+++ src/main/java/com/custom/site/name/decoder/ThirdLevelDecoder.java
@@ -12,10 +12,10 @@ import java.util.List;
 public class ThirdLevelDecoder {
    
    public long[][] decodeVector(List<Long> vector) {
-      long[] blockAsArray = new long[64];
       long[][] block = new long[8][8];
       List<Integer> posx = new ArrayList<Integer>();
       List<Integer> posy = new ArrayList<Integer>();
+      
       // build of posx
       for (int i = 0; i < block.length; i += 2) {
          int j;
@@ -52,28 +52,31 @@ public class ThirdLevelDecoder {
             posy.add(j);
       }
       
-      // creating the vector
-      for (int i = 0; i < posx.size(); i++)
-         vector.add(block[posx.get(i)][posy.get(i)]);
-      
-      for (int i = 0; i < 64; i++)
-         blockAsArray[i] = 0;
-      
       for (int i = 0; i < block.length; i++)
          for (int j = 0; j < block.length; j++)
             block[i][j] = 0;
          
+      int temp = 1;
       block[0][0] = vector.get(1);
-      for (int i = 1; i < posx.size(); i++) {
-         int tempI = i;
-         if ((tempI * 3 + 1) > vector.size())
+      for (int i = 1;; i++) {
+         // int tempI = i;
+         // if ((tempI * 3 + 1) > (vector.size() - 1))
+         // break;
+         
+         i += vector.get(3 * temp - 1);
+         // while (vector.get(tempI * 3 - 1) > 0 && (i * 3 + 1) < posx.size()) {
+         // block[posx.get(i)][posy.get(i)] = 0;
+         // vector.set(tempI * 3 - 1, vector.get(tempI * 3 - 1) - 1);
+         // i++;
+         // }
+         if (i == 23)
+            System.out.println((i * 3 + 1) + "*" + vector.size());
+         if (i == 23)
+            System.out.println((i * 3 + 1) + "?" + vector.size());
+         block[posx.get(i)][posy.get(i)] = vector.get(3 * temp + 1);
+         temp += 1;
+         if ((temp * 3 + 1) > vector.size() - 1)
             break;
-         while (vector.get(tempI * 3 - 1) > 0 && (i * 3 + 1) < posx.size()) {
-            block[posx.get(i)][posy.get(i)] = 0;
-            vector.set(tempI * 3 - 1, vector.get(tempI * 3 - 1) - 1);
-            i++;
-         }
-         block[posx.get(i)][posy.get(i)] = vector.get(3 * i + 1);
       }
       return block;
    }
